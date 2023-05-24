8fa5b06bbaa7dca2cb5647a71e3d7bdf9e883b3e
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-05-24 21:43:57 +0300

    methods

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] messages = { "Hello World!", "Codecool is best." };
+            PrintMessage(messages);
+
+            PrintMessages("Hello Codecool");
+
+            void PrintMessages(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        private static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private static void PrintMessage(string[] messages)
+        {
+            foreach (var message in messages)
+            {
+                Print(message);
+            }
+
+            void Print(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
