7d840e054b9ece1b0f1479281434560896e13abf
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-05-24 22:32:09 +0300

    parsing strings 1st method

---
+++ Parsing strings/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace parsingStrings
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            string input = GetInput();
+
+            int number = ParseInput(input);
+
+            Console.WriteLine("The square of the number:");
+            Console.WriteLine(SquareInput(number));
+
+            Console.ReadKey();
+        }
+
+        private static string GetInput()
+        {
+            return Console.ReadLine() ?? "";
+        }
+
+        private static int ParseInput(string input)
+        {
+            return Int32.Parse(input);
+        }
+
+        private static int SquareInput(int input)
+        {
+            return input * input;
+        }
+    }
+}
